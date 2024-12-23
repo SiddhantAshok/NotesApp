1e57a0a88c9484cb98e9c97c0cbc112e5d70b075
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2024-12-23 23:35:58 +0530

    Notes Application Dotnet 6 API

---
+++ Notes/Notes.API/Controllers/NotesController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using Notes.API.Data;
+using Notes.API.Models.Entities;
+
+namespace Notes.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly NotesDBContext _context;
+
+        public NotesController(NotesDBContext context)
+        {
+                _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            //Get the notes from the database
+            var notes = await _context.Notes.ToListAsync();
+
+            return Ok(notes);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        [ActionName("GetNoteById")]
+        public async Task<IActionResult> GetNoteById([FromRoute] Guid id)
+        {
+            //Get the note from the database
+            var note = await _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNote([FromBody] Note note)
+        {
+            //Add the note to the database
+            note.Id = Guid.NewGuid();
+            await _context.Notes.AddAsync(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetNoteById), new { id = note.Id }, note);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateNote([FromRoute] Guid id, [FromBody] Note note)
+        {
+            //Get the note from the database
+            var existingNote = await _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+
+            //Update the note
+            existingNote.Title = note.Title;
+            existingNote.Description = note.Description;
+            existingNote.IsVisible = note.IsVisible;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(existingNote);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteNote([FromRoute] Guid id)
+        {
+            //Get the note from the database
+            var note = await _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            //Delete the note
+            _context.Notes.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Notes/Notes.API/Data/NotesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.API.Models.Entities;
+
+namespace Notes.API.Data
+{
+    public class NotesDBContext : DbContext
+    {
+        public NotesDBContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
